103ca4456a7659774ba5fbcb3d52e9bf288510ab
Michael <dammydeji.dd@gmail.com>
2025-02-03 16:49:17 +0100

    feat/CartController created

---
+++ src/main/java/com/dammy/bookstoreapi/controller/CartController.java
@@ -0,0 +1,31 @@
+package com.dammy.bookstoreapi.controller;
+
+import com.dammy.bookstoreapi.model.Book;
+import com.dammy.bookstoreapi.model.ShoppingCart;
+import com.dammy.bookstoreapi.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/cart")
+public class CartController {
+    @Autowired
+    private CartService cartService;
+
+    @PostMapping
+    public ShoppingCart createCart() {
+        return cartService.createCart();
+    }
+
+    @PostMapping("/{cartId}/add")
+    public ShoppingCart addBookToCart(@PathVariable Long cartId, @RequestBody Book book) {
+        return cartService.addBookToCart(cartId, book);
+    }
+
+    @GetMapping("/{cartId}")
+    public Optional<ShoppingCart> getCart(@PathVariable Long cartId) {
+        return cartService.getCart(cartId);
+    }
+}

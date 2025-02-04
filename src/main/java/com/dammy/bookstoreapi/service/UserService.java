ea7d36ad545b9e46716de10c058f4fc184ea5987
Michael <dammydeji.dd@gmail.com>
2025-02-04 06:00:11 +0100

    feat/User service added

---
+++ src/main/java/com/dammy/bookstoreapi/service/UserService.java
@@ -0,0 +1,24 @@
+package com.dammy.bookstoreapi.service;
+
+import com.dammy.bookstoreapi.model.User;
+import com.dammy.bookstoreapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public User register(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public User findUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}

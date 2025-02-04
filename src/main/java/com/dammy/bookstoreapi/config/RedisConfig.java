d702ff46d65f9e746d6c959975d0d30bf5c967d4
Michael <dammydeji.dd@gmail.com>
2025-02-04 16:13:49 +0100

    Redis config and application properties modified

--- src/main/java/com/dammy/bookstoreapi/config/RedisConfig.java
+++ src/main/java/com/dammy/bookstoreapi/config/RedisConfig.java
@@ -3,6 +3,8 @@ package com.dammy.bookstoreapi.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -12,7 +14,9 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("redis", 6379);
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("redis", 6379);
+        config.setPassword(RedisPassword.of("redis_password"));
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean
